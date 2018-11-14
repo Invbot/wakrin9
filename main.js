d92b9cdb1cacb3677e118e3cb4199b53a9b275b0
Invbot <44956312+Invbot@users.noreply.github.com>
2018-11-14 18:20:31 -0500

    Update main.js

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ function sleep(milliSeconds) {
     client.on("message", message => {
 
     client.on('guildMemberAdd', member => {
-      message.guild.channels.get('427164056542117888').send({embed: {
+      message.guild.channels.get('512076563831848993').send({embed: {
 color: 3447003,
 author: {
   name: member.user.username,
