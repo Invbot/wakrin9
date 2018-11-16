632d485720523be9331b690700ad3ba7dce3c1fc
Invbot <44956312+Invbot@users.noreply.github.com>
2018-11-16 18:32:35 -0500

    Update main.js

--- main.js
+++ main.js
@@ -45,7 +45,6 @@ client.on("guildMemberAdd", (member) => {
   const guild = member.guild;
   if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
   newUsers[guild.id].set(member.id, member.user);
-​
   if (newUsers[guild.id].size > 10) {
     const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
     guild.channels.find(channel => channel.name === "general").send("Welcome our new users!\n" + userlist);
