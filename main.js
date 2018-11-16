78b5ca12f182a49c4e5702a7145703253c579cc9
Invbot <44956312+Invbot@users.noreply.github.com>
2018-11-16 18:31:20 -0500

    Update main.js

--- main.js
+++ main.js
@@ -40,18 +40,17 @@ function sleep(milliSeconds) {
 
     client.on("message", message => {
 
-   client.on('guildMemberAdd', member => {
- let channel = message.guild.channels.get('512763014135414796');
 
-  let embed = new Discord.RichEmbed()
-    .setMaxListeners()
-    .setTitle("Bienvenue")
-    .setColor('RANDOM')
-    .setDescription(`Hey ${member} !, bienvenue sur le serveur !`)
-    .addField('Le serveur contient actuellement :', `${message.guild.members.size} membres !`)
-    .setFooter(`© InVulsBot`)
-    .setThumbnail(message.author.avatarURL);
-  channel.send({embed});
+client.on("guildMemberAdd", (member) => {
+  const guild = member.guild;
+  if (!newUsers[guild.id]) newUsers[guild.id] = new Discord.Collection();
+  newUsers[guild.id].set(member.id, member.user);
+​
+  if (newUsers[guild.id].size > 10) {
+    const userlist = newUsers[guild.id].map(u => u.toString()).join(" ");
+    guild.channels.find(channel => channel.name === "general").send("Welcome our new users!\n" + userlist);
+    newUsers[guild.id].clear();
+  }
 });
    
    if (message.content.includes("https://")) {
