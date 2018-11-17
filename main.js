23ab45e110b9fe5bb92946b277b07daa2b26431b
Invbot <44956312+Invbot@users.noreply.github.com>
2018-11-17 13:41:20 -0500

    Update main.js

--- main.js
+++ main.js
@@ -38,10 +38,10 @@ function sleep(milliSeconds) {
     });
 
 
-    client.on("message", async message) => {
+    client.on("message", async message => {
      client.on("guildMemberAdd", member => {
   console.log(`New User ${member.user.username} has joined ${member.guild.name}` );
-  meessage.channel.send(`${member.user} à rejoins le serveur ! Nous sommes actuellement ${member.guild.size}`);
+  message.channel.send(`${member.user} à rejoins le serveur ! Nous sommes actuellement ${member.guild.size}`);
 });
    if (message.content.includes("https://")) {
       if (message.channel.id ===  '481194800133963785') return
