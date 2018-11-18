2004f36b23a0cdc1ba863c19a18d5bdffca15b85
Invbot <44956312+Invbot@users.noreply.github.com>
2018-11-17 22:03:58 -0500

    Update main.js

--- main.js
+++ main.js
@@ -40,7 +40,6 @@ bot.on("guildMemberAdd", function(member) {
         .setColor('RANDOM')
         .setDescription(`Hey! ${member} Bienvenue sur le serveur ! Si tu as des questions n'hésite pas.\nNous sommes actuellement ${member.guild.members.size}`)
         .setFooter("© InVulsBot", "https://images-ext-2.discordapp.net/external/QELomkRUVhZuLIDbQI6220WSAhRAwnD0Dg-igmd02to/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/493471628043354133/ab0970c55a68235387ab2695d825b6f9.png")
-        .setThumbnail(member.author.avatarURL)
         .setTimestamp();
         member.guild.channels.get('513536593529798754').send(embed);
         });
