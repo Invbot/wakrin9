bf01ffbeab4b18f1c817cbebd512aefe4e8378e8
Invbot <44956312+Invbot@users.noreply.github.com>
2018-11-17 21:43:29 -0500

    Update main.js

--- main.js
+++ main.js
@@ -44,18 +44,17 @@ function clean(text) {
         
     bot.on("message", (message) => {
    
-    bot.on("guildMemberAdd", function(member) {
-          
- let channel = message.guild.channels.get('513536593529798746');
-
-  let embed = new Discord.RichEmbed()
-    .setTitle("Bienvenue")
-    .setColor('RANDOM')
-    .setDescription(`Hey ${member} !, Bienvenue sur le serveur !`)
-    .addField('Le serveur contient actuellement :', `${member.guild.members.size} membres !`)
-    .setFooter(`© InVulsBot`)
-  channel.send({embed});
-});
+        bot.on("guildMemberAdd", member => {
+        const embed = new Discord.RichEmbed()
+        .setTitle("Nouvel arrivant :")
+        .setAuthor("InVuls Bot", "https://images-ext-2.discordapp.net/external/QELomkRUVhZuLIDbQI6220WSAhRAwnD0Dg-igmd02to/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/493471628043354133/ab0970c55a68235387ab2695d825b6f9.png")
+        .setColor('RANDOM')
+        .setDescription(`Hey! ${member} Bienvenue sur le serveur ! Si tu as des questions n'hésite pas.\nNous sommes actuellement ${message.guild.members.size}`)
+        .setFooter("© InVulsBot", "https://images-ext-2.discordapp.net/external/QELomkRUVhZuLIDbQI6220WSAhRAwnD0Dg-igmd02to/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/493471628043354133/ab0970c55a68235387ab2695d825b6f9.png")
+        .setThumbnail(message.author.avatarURL)
+        .setTimestamp();
+        message.channel.send({embed});
+        });
             
    if (message.content.includes("https://")) {
       if (message.channel.id ===  '481194800133963785') return
