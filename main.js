4016a15dfe5905205ccf0815c11ad1ef24ecc3f9
Invbot <44956312+Invbot@users.noreply.github.com>
2018-11-22 21:41:26 -0500

    Update main.js

--- main.js
+++ main.js
@@ -37,7 +37,8 @@ bot.on("guildMemberAdd", function(member) {
         .setTitle("Nouvel arrivant :")
         .setColor('RANDOM')
         .setDescription(`__Bienvenue à ${member.user.username}__\n**Grâce à toi, nous sommes actuellement ${member.guild.memberCount}**`)
-        .setFooter(`${member.guild.name}`, "\n:flag_fr:")
+        .addField(`${member.guild.name}`, "- :flag_fr:")
+         .setFooter(`© InVulsBot`, "https://images-ext-2.discordapp.net/external/QELomkRUVhZuLIDbQI6220WSAhRAwnD0Dg-igmd02to/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/493471628043354133/ab0970c55a68235387ab2695d825b6f9.png")
         .setTimestamp();
         member.guild.channels.get('351629980570091531').send(embed);
         });
