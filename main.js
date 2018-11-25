8073ae470f6d68cd0d1c9e9a0f4ecdd769e5e949
Invbot <44956312+Invbot@users.noreply.github.com>
2018-11-25 18:48:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -312,7 +312,7 @@ if(message.mentions.users.size === 0) {
             .setColor("#FF0000")
             .setTitle("Warn :")
             .addField("Membre warn:", `${target.user.tag}\n(${target.user.id})`, true)
-	    .addField("Raison :", `${reason}`)
+	    .addField('Raison :', `${reason}`)
             .addField("ID :", `${target.user.id}`)
             .addField("Modérateur :", `${message.author.username}`)
             message.guild.channels.find('name', 'logs').send(warn_embed);
