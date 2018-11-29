4da1ce8ea0bfa70119da65ac1bcfdbda771c814d
Invbot <44956312+Invbot@users.noreply.github.com>
2018-11-29 16:49:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -920,7 +920,7 @@ if(message.content === prefix+'InvulsCode'){
 let role = message.guild.roles.find('name', 'Community');
 
 if(message.content === prefix + "create"){
-message.member.addRole("name", "Solo duel");
+message.member.find("name", "Solo duel").addRole;
 }
             
             if(message.content === prefix+"duel"){
