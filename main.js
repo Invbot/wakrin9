d6a640ed3ff9054c24481a51147e1b8b5ce40b6c
Invbot <44956312+Invbot@users.noreply.github.com>
2018-12-01 11:36:21 -0500

    Update main.js

--- main.js
+++ main.js
@@ -985,7 +985,65 @@ if(message.content === prefix+'InvulsCode'){
        message.channel.send(dueltest)
         break;       
   }
-  
+  const yourID = "363762795801477120","282209791265472512"; //Instructions on how to get this: https://redd.it/40zgse
+const setupCMD = "!roles"
+let initialMessage = `**Régissez avec les réactions ci-dessous pour vous attribuez le rôle. Pour enlever le rôle, enlevez votre réaction!**`;
+const roles = ["XBOX", "PS4", "PC", "Snipe"];
+const reactions = ["💻", "🖌", "😃", "🆕"];
+
+if (roles.length !== reactions.length) throw "La liste des rôles et des réactions n'ont pas la même grandeur!";
+
+//Function to generate the role messages, based on your settings
+function generateMessages(){
+    var messages = [];
+    messages.push(initialMessage);
+    for (let role of roles) messages.push(`Réagissez pour avoir le role **"${role}"**`); //DONT CHANGE THIS
+    return messages;
+}
+
+
+bot.on("message", message => {
+    if (message.author.id == yourID && message.content.toLowerCase() == setupCMD){
+        var toSend = generateMessages();
+        let mappedArray = [[toSend[0], false], ...toSend.slice(1).map( (message, idx) => [message, reactions[idx]])];
+        for (let mapObj of mappedArray){
+            message.channel.send(mapObj[0]).then( sent => {
+                if (mapObj[1]){
+                  sent.react(mapObj[1]);  
+                } 
+            });
+        }
+    }
+})
+
+
+bot.on('raw', event => {
+    if (event.t === 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
+        
+        let channel = bot.channels.get(event.d.channel_id);
+        let message = channel.fetchMessage(event.d.message_id).then(msg=> {
+        let user = msg.guild.members.get(event.d.user_id);
+        
+        if (msg.author.id == bot.user.id && msg.content != initialMessage){
+       
+            var re = `\\*\\*"(.+)?(?="\\*\\*)`;
+            var role = msg.content.match(re)[1];
+        
+            if (user.id != bot.user.id){
+                var roleObj = msg.guild.roles.find(r => r.name === role);
+                var memberObj = msg.guild.members.get(user.id);
+                
+                if (event.t === "MESSAGE_REACTION_ADD"){
+                    memberObj.addRole(roleObj)
+                } else {
+                    memberObj.removeRole(roleObj);
+                }
+            }
+        }
+        })
+ 
+    }   
+            
 });
 
   bot.login(process.env.TOKEN);
