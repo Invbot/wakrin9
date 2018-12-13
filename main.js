d5e07218f6265fcca45acf98f8d1063383a0dd29
Invbot <44956312+Invbot@users.noreply.github.com>
2018-12-12 19:11:10 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1167,5 +1167,34 @@ bot.on('raw', event => {
  
     }
 });
+      if (message.content === prefix  + "smoke")
+
+    message.channel.send('**BISSSSHES IM SMOKING**').then(async message => {
+        setTimeout(() => {
+            message.edit('🚬');
+        }, 500);
+        setTimeout(() => {
+            message.edit('🚬 ☁ ');
+        }, 1000);
+        setTimeout(() => {
+            message.edit('🚬 ☁☁ ');
+        }, 1500);
+        setTimeout(() => {
+            message.edit('🚬 ☁☁☁ ');
+        }, 2000);
+        setTimeout(() => {
+            message.edit('🚬 ☁☁');
+        }, 2500);
+        setTimeout(() => {
+            message.edit('🚬 ☁');
+        }, 3000);
+        setTimeout(() => {
+            message.edit('🚬 ');
+        }, 3500);
+        setTimeout(() => {
+            message.edit(`J'ai fini !`);
+        }, 4000);
+    });
+};
 
     });
