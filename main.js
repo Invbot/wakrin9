0d808ae0e745446489922e50b514b65212834f77
Invbot <44956312+Invbot@users.noreply.github.com>
2018-12-15 11:01:48 -0500

    Update main.js

--- main.js
+++ main.js
@@ -35,6 +35,20 @@ function clean(text) {
     else
         return text;
 }
+const antispam = require("discord-anti-spam");
+ 
+antispam(bot, {
+  warnBuffer: 3, //Maximum amount of messages allowed to send in the interval time before getting warned.
+  maxBuffer: 5, // Maximum amount of messages allowed to send in the interval time before getting banned.
+  interval: 1000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
+  warningMessage: "Arrête de spam !", // Warning message send to the user indicating they are going to fast.
+  banMessage: "A été ban pour spam. Quelqu'un d'autre ?", // Ban message, always tags the banned user in front of it.
+  maxDuplicatesWarning: 7,// Maximum amount of duplicate messages a user can send in a timespan before getting warned
+  maxDuplicatesBan: 10, // Maximum amount of duplicate messages a user can send in a timespan before getting banned
+  deleteMessagesAfterBanForPastDays: 7 // Delete the spammed messages after banning for the past x days.
+  exemptRoles: ["Manager"] // The names of the roles which should not be spam-filtered
+  exemptUsers: ["InVuls DojoGuigi#8893", "InVuls TxZ#0954", "🎄- [InVuls Dev]AŁع〤0754#0081"] // The Discord tags of the users who should not be spam-filtered
+});
 
 
 
