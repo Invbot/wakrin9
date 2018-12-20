ed8a844bb760a39805e9d8840be8b1af0925824c
Invbot <44956312+Invbot@users.noreply.github.com>
2018-12-20 18:54:55 -0500

    Update main.js

--- main.js
+++ main.js
@@ -168,7 +168,7 @@ const commands = {
       //système de sécurité
  if (command === "lockdown") {
   if (message.member.hasPermission("ADMINISTRATOR")) {
-   if (!client.lockit) client.lockit = [];
+   if (!bot.lockit) bot.lockit = [];
   let time = args.join(' ');
   let validUnlocks = ['release', 'unlock'];
   if (!time) return message.reply('You must set a duration for the lockdown in either hours, minutes or seconds');
@@ -177,8 +177,8 @@ const commands = {
       SEND_MESSAGES: null
     }).then(() => {
       message.channel.sendMessage('Lockdown lifted.');
-      clearTimeout(client.lockit[message.channel.id]);
-      delete client.lockit[message.channel.id];
+      clearTimeout(bot.lockit[message.channel.id]);
+      delete bot.lockit[message.channel.id];
     }).catch(error => {
       console.log(error);
     });
@@ -188,11 +188,11 @@ const commands = {
     }).then(() => {
       message.channel.sendMessage(`Channel locked down for ${ms(ms(time), { long:true })}`).then(() => {
  
-        client.lockit[message.channel.id] = setTimeout(() => {
+        bot.lockit[message.channel.id] = setTimeout(() => {
           message.channel.overwritePermissions(message.guild.id, {
             SEND_MESSAGES: null
           }).then(message.channel.sendMessage('Lockdown lifted.')).catch(console.error);
-          delete client.lockit[message.channel.id];
+          delete bot.lockit[message.channel.id];
         }, ms(time));
  
       }).catch(error => {
