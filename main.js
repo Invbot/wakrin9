d5dc6abfaabe2f7a43c808d6fc0d1a421731ccd9
Invbot <44956312+Invbot@users.noreply.github.com>
2019-01-02 11:29:23 +0100

    Debug welcome channel (by Spileur)

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ bot.on("guildMemberAdd", function(member) {
         .addField(`**Nous sommes ${member.guild.memberCount} grâce à toi !**`,`InVuls E-Sport / Communauté - :flag_fr:`)
         .setFooter(`© InVulsBot`, "https://images-ext-2.discordapp.net/external/QELomkRUVhZuLIDbQI6220WSAhRAwnD0Dg-igmd02to/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/493471628043354133/ab0970c55a68235387ab2695d825b6f9.png")
         .setTimestamp();
-        member.guild.channels.get('351629980570091531').send(embed);
+        if(member.guild.channels.get('351629980570091531')) member.guild.channels.get('351629980570091531').send(embed);
         });
 
 
